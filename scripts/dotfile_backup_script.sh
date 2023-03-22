db9db0c5791b94eca4558678c061450afe814b8e
Gil Fewster <GilFewster@users.noreply.github.com>
2023-03-22 18:12:52 +1100

    automatic update: 22-03-2023 at 18:12:52

--- scripts/dotfile_backup_script.sh
+++ scripts/dotfile_backup_script.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
 # This script helps me update my dot files regularly
-
 cd ${HOME}/dotfile_backup/
 
 timestamp() {
   date +"%d-%m-%Y at %T"
 }
 
-grep -v '^$\|^\s*\#' $BACKUP | while read -r line; do
-	echo "$line" | awk '{ system("rsync " $1 " " $2) }'
+# while IFS= read -r line; do
+#     echo "Text read from file: $line"
+# done < grep -v '^$\|^\s*\#' | scripts/backup_list
+
+grep -v '^$\|^\s*\#' scripts/backup_list | while read -r line; do
+	echo "$line"
 done
+
+# grep -v '^$\|^\s*\#' $BACKUP | while read -r line; do
+# 	echo "$line" | awk '{ system("rsync " $1 " " $2) }'
+# done
 
 if [[ `git status --porcelain` ]]; then
 	{
